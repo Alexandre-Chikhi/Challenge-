3403256d3a627b39483c3e0ce7fb37cfc7cb8280
7611488G <alexandre.chikhi@gmail.com>
2022-09-21 14:19:00 +0200

    express08

--- userHandlers.js
+++ userHandlers.js
@@ -76,12 +76,13 @@ const getUsers = (req, res) => {
   };
 
 const postUser = (req, res) => {
-    const { firstname, lastname, email, city, language } = req.body;
+
+    const { firstname, lastname, email, city, language, Hpassword } = req.body;
     
     database
     .query(
-      "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
-      [firstname, lastname, email, city, language]
+      "INSERT INTO users(firstname, lastname, email, city, language, hashedPassword) VALUES (?, ?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language, Hpassword]
     )
     .then(([result]) => {
       res.location(`/api/users/${result.insertId}`).sendStatus(201);
